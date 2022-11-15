aa98be2ecb13fc5dc653fd6790da5313f60269ba
dajeong09 <chic519@gmail.com>
2022-11-15 21:54:14 +0900

    feat : 로그인 기능 및 회원별로 정보를 다르게 표시할 수 있도록 구현

--- src/main/java/com/sparta/myselectshop/security/WebSecurityConfig.java
+++ src/main/java/com/sparta/myselectshop/security/WebSecurityConfig.java
@@ -5,11 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity // 스프링 Security 지원을 가능하게 함
 public class WebSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
@@ -20,9 +27,8 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // 회원 관리 처리 API (POST /user/**) 에 대해 CSRF 무시
-        http.csrf()
-                .ignoringAntMatchers("/user/**");
+
+        http.csrf().disable();
 
         http.authorizeRequests()
                 // image 폴더를 login 없이 허용
@@ -36,13 +42,17 @@ public class WebSecurityConfig {
                 .and()
                 // 로그인 기능 허용
                 .formLogin()
+                // 로그인 View 제공 (GET /user/login)
                 .loginPage("/user/login")
+                // 로그인 처리 (POST /user/login)
+                .loginProcessingUrl("/user/login")
                 .defaultSuccessUrl("/")
                 .failureUrl("/user/login?error")
                 .permitAll()
                 .and()
                 // 로그아웃 기능 허용
                 .logout()
+                .logoutUrl("/user/logout")
                 .permitAll();
 
         return http.build();
--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -4,43 +4,43 @@ import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.model.Product;
 import com.sparta.myselectshop.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import java.sql.SQLException;
 import java.util.List;
 
+@Service
 public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public ProductService() {
-        ProductRepository productRepository = new ProductRepository();
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
-    public Product createProduct(ProductRequestDto requestDto) throws SQLException {
+    public Product createProduct(ProductRequestDto requestDto, Long userId ) {
         // 요청받은 DTO 로 DB에 저장할 객체 만들기
-        Product product = new Product(requestDto);
+        Product product = new Product(requestDto, userId);
 
-        productRepository.createProduct(product);
+        productRepository.save(product);
 
         return product;
     }
 
-    public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) throws SQLException {
-        Product product = productRepository.getProduct(id);
-        if (product == null) {
-            throw new NullPointerException("해당 아이디가 존재하지 않습니다.");
-        }
+    public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new NullPointerException("해당 아이디가 존재하지 않습니다."));
 
         int myprice = requestDto.getMyprice();
-        productRepository.updateMyprice(id, myprice);
+        product.setMyprice(myprice);
+        productRepository.save(product);
 
         return product;
     }
 
-    public List<Product> getProducts() throws SQLException {
-        List<Product> products = productRepository.getProducts();
-
-        return products;
+    // 회원 ID 로 등록된 상품 조회
+    public List<Product> getProducts(Long userId) {
+        return productRepository.findAllByUserId(userId);
     }
 }
